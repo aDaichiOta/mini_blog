503bdb699f37cfb2c485656116524ba456ba8257
skirino <skirino@rorhandson52.(none)>
2013-05-16 16:52:51 +0900

    Add authorization setting for rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -201,4 +201,8 @@ RailsAdmin.config do |config|
       end
     end
   end
+
+  config.authorize_with do |controller|
+    redirect_to main_app.root_path unless current_user.try(:admin?)
+  end
 end
